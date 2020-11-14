b7918d60d559ad9f8b869dc86035994dfda0d4a1
Student D03 EE364 <ee364d03@ecegrid-thin7.ecn.purdue.edu>
2020-11-14 17:04:07 -0500

    added post template, posting works, profile for loop added and works but not aligned properly

--- data/create_table.sql
+++ data/create_table.sql
@@ -1,10 +1,12 @@
 
 PRAGMA foreign_keys = OFF;
 DROP TABLE users;
-DROP TABLE Posts;
+DROP TABLE posts;
+DROP TABLE votes;
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, hash INTEGER);
-CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY ASC, poster CONSTRAINT cons REFERENCES users(username), title TEXT, content TEXT, );
+CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY, poster CONSTRAINT cons REFERENCES users(username), title TEXT, content TEXT, score INTEGER, dateposted TEXT);
+CREATE TABLE IF NOT EXISTS votes(id INTEGER PRIMARY KEY ASC, postid CONSTRAINT cons REFERENCES posts(id), username CONSTRAINT cons REFERENCES users(username), vote INTEGER);
 
 SELECT * FROM users;
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import flask
 import pysqlite3 as sqlite3
 import sqlite3_helper
 import config
+from datetime import datetime
 
 # ######################################################
 # No Module - Level Variables or Statements !
@@ -18,89 +19,46 @@ hdb = sqlite3_helper.SqliteDB(db_path=config.PATH_DATABASE, row_type="dict", pla
 @app.route("/<hashedcode>/profile", methods=['GET', 'POST'])
 def profile(hashedcode):
     username = hdb.select(["users"], what="username", hash=int(hashedcode))[0]['username']
-    return flask.render_template('profile.html', username=username)
+    posts = hdb.select(['posts'], poster=username)
+    return flask.render_template('profile.html', username=username, hashedcode=hashedcode, posts=posts)
 
-@app.route("/create", methods=['GET', 'POST'])
-def create():
-    return flask.render_template('create.html')
+@app.route("/<hashedcode>/create", methods=['GET', 'POST'])
+def create(hashedcode):
+    return flask.render_template('create.html', hashedcode=hashedcode)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     return flask.render_template('register.html')
+
+@app.route("/posts/<postid>", methods=['GET', 'POST'])
+def post(postid):
+    row = hdb.select_row(['posts'], id=int(postid))
+    return flask.render_template('post.html', poster=row['poster'], title=row['title'], content=row['content'], dateposted=row['dateposted'], score=row['score'])
 
 @app.route("/<hashedcode>/feed", methods=['GET', 'POST'])
 def feed(hashedcode):
     username = hdb.select(["users"], what="username", hash=int(hashedcode))[0]['username']
     return flask.render_template('feed.html', username=username, hashedcode=int(hashedcode))
 
-@app.route("/submit", methods=['POST'])
+@app.route("/submit_register", methods=['POST'])
 def submit_register():
-    if len(hdb.select(["users"], what="username", username=flask.request.form.get('username'))) == 0:
-        hashedcode = hash(flask.request.form.get('username'))
-        hdb.insert("users", username=flask.request.form.get('username'), hash=hashedcode)
-        # print(hdb.select(['users']))
-    else:
-        hashedcode = hdb.select(["users"], what="hash", username=flask.request.form.get('username'))[0]['hash']
-    print(hdb.select(['users']))
-    return flask.redirect(flask.url_for(".feed", hashedcode=str(hashedcode)))
+    try:
+        row = hdb.select_row(('users'), username=flask.request.form.get('username'))
+    except sqlite3_helper.DBItemNotFoundError:
+        hdb.insert(('users'), username=flask.request.form.get('username'), hash=hash(flask.request.form.get('username')))
+        row = hdb.select_row(('users'), username=flask.request.form.get('username'))
+    return flask.redirect(flask.url_for(".feed", hashedcode=row['hash']))
 
-# @app.route("/form_submit", methods=['POST'])
-# def submit_form():
-#     if flask.request.method == 'POST':
-#         conn = sqlite3.connect("data/company.db")
-#         s = "INSERT INTO employees VALUES ('" + str(flask.request.form.get('first')) +"','" + str(flask.request.form.get('last')) + "','" + str(flask.request.form.get('id')) + "','" + str(flask.request.form.get('position')) + "')"
-#         conn.execute(s)
-#         conn.commit()
-#         conn.close()
-#         return flask.redirect(flask.url_for(".home"))
-#
-# @app.route("/delete", methods=['POST'])
-# def delete():
-#     if flask.request.method == 'POST':
-#         conn = sqlite3.connect("data/company.db")
-#         s = "DELETE FROM employees WHERE id=" + str(flask.request.args.get('emp_id')) + ";"
-#         conn.execute(s)
-#         conn.commit()
-#         conn.close()
-#         return flask.redirect(flask.url_for(".home"))
-#
-# @app.route("/view", methods=['GET'])
-# def view():
-#     conn = sqlite3.connect("data/company.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM employees WHERE id=" + str(flask.request.args.get('emp_id')) + ";")
-#     record = cur.fetchall()[0]
-#     first_name = record[0]
-#     last_name = record[1]
-#     id = record[2]
-#     position = record[3]
-#     return flask.render_template("view.html", first_name = first_name,  last_name = last_name, position = position, id = str(id))
-#
-# @app.route("/view/<emp_id>", methods=['GET'])
-# def view2(emp_id):
-#     conn = sqlite3.connect("data/company.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM employees WHERE id=" + str(emp_id) + ";")
-#     record = cur.fetchall()[0]
-#     first_name = record[0]
-#     last_name = record[1]
-#     id = record[2]
-#     position = record[3]
-#     return flask.render_template("view.html", first_name = first_name,  last_name = last_name, position = position, id = str(id))
-#
-# @app.route("/view_all", methods=['GET'])
-# def view_all():
-#     conn = sqlite3.connect("data/company.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM employees")
-#     records = cur.fetchall()
-#     return flask.render_template("view_all.html", records = records)
+@app.route("/<hashedcode>/submit_post", methods=['POST'])
+def submit_post(hashedcode):
+    username = hdb.select_row(["users"], what="username", hash=int(hashedcode))['username']
+    rowid = hdb.insert("posts", return_row_id=True, poster=username, title=flask.request.form.get('title'), content=flask.request.form.get('content'), score=0, dateposted=datetime.now().isoformat())
+    print(hdb.select(['posts'])) # don't get rid of this
+    return flask.redirect(flask.url_for(".post", postid=rowid))
 
 
 # This block is optional and can be used for testing .
 # We will NOT look into its content .
 # ######################################################
 if __name__ == "__main__":
-    # hdb.insert("users", username='sriniv62', hash="", id=5)
-    # print(hdb.select(["users"], what="*", order="id DESC", limit=1))
     app.run()
