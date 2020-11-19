1bcbe60cc0cbacf132434a171ceafee9c2915114
Student D03 EE364 <ee364d03@ecegrid-thin7.ecn.purdue.edu>
2020-11-19 17:54:59 -0500

    fixed score display on posts api

--- scripts/post.js
+++ scripts/post.js
@@ -5,25 +5,30 @@ $(document).ready(function(){
     if($('#data').data("uservote") == -1) {
         $("#downvote").attr("src", "../../../static/data/Downvoted.png")
     }
+    $("#score").html("&nbsp;&nbsp;" + $('#data').data("score"))
     $("#upvote").click(function() {
         if($('#upvote').attr("src") == '../../../static/data/Upvoted.png') {
             $("#upvote").attr("src", "../../../static/data/Upvote.png")
+            $("#score").html("&nbsp;&nbsp;" + ($('#data').data("score")))
             $.post("/vote", {"voterHash": $('#data').data("userhash"), "vote": 0, "postid": $('#data').data("postid")})
         }
         else {
             $("#upvote").attr("src", '../../../static/data/Upvoted.png')
             $("#downvote").attr("src", "../../../static/data/Downvote.png")
+            $("#score").html("&nbsp;&nbsp;" + ($('#data').data("score") + 1))
             $.post("/vote", {"voterHash": $('#data').data("userhash"), "vote": 1, "postid": $('#data').data("postid")})
         }
     });
     $("#downvote").click(function() {
         if($('#downvote').attr("src") == '../../../static/data/Downvoted.png') {
             $("#downvote").attr("src", '../../../static/data/Downvote.png')
+            $("#score").html("&nbsp;&nbsp;" + ($('#data').data("score")))
             $.post("/vote", {"voterHash": $('#data').data("userhash"), "vote": 0, "postid": $('#data').data("postid")})
         }
         else {
             $("#downvote").attr("src", '../../../static/data/Downvoted.png')
             $("#upvote").attr("src", '../../../static/data/Upvote.png')
+            $("#score").html("&nbsp;&nbsp;" + ($('#data').data("score") - 1))
             $.post("/vote", {"voterHash": $('#data').data("userhash"), "vote": -1, "postid": $('#data').data("postid")})
         }
     });
